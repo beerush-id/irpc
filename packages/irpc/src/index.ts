0c7c661175959f3e4a29ba0cc19e0cde9ba9cded
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2025-11-04 17:13:04 +0700

    refactor(irpc): remove unused createModule export
    - Removed createModule import and export from index.ts
    - Deleted redundant empty line at end of file
    
    refactor(http): rename package from http-transport to http
    - Updated package name in bun.lock
    - Changed directory reference in workspace configuration

--- packages/irpc/src/index.ts
+++ packages/irpc/src/index.ts
@@ -1,10 +1,6 @@
-import { createModule } from './module.js';
-
 export * from './batch.js';
 export * from './call.js';
 export * from './context.js';
 export * from './module.js';
 export * from './types.js';
 export * from './utils.js';
-
-export const irpc = createModule();
